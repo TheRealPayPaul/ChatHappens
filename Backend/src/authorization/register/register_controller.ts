ac436fcc2f43759d5b90593e386983a3efc1aa8a
Paul Panosch <paulpanosch@gmx.at>
2023-04-29 15:30:56 +0200

    Refactored with controllers. Works like a charm :)

---
+++ Backend/src/authorization/register/register_controller.ts
@@ -0,0 +1,56 @@
+import { ErrorCode, StatusCode } from "../../core/codes";
+import Controller from "../../core/controller";
+import UserService from "../../core/service/user_service";
+import UserDTO from "./user_dto";
+import { Request, Response } from "express";
+import bcrypt from 'bcrypt';
+
+export default class RegisterController extends Controller {
+
+    public static async handle(req: Request, res: Response) {
+        let user = new UserDTO(req.body);
+
+        // Check if data from client is valid
+        if (!user.isValidCreateUser()) {
+            this.sendError({
+                Message: user.getErrors(),
+                ErrorCode: ErrorCode.SENT_DATA_INVALID,
+                StatusCode: StatusCode.BAD_REQUEST,
+            }, res);
+            return;
+        }
+
+        // Check if email already exists
+        const emailExists = await UserService.getByEmail(user.email as string);
+        if (emailExists) {
+            this.sendError({
+                Message: `Email '${user.email}' already exists!`,
+                ErrorCode: ErrorCode.EMAIL_ALREADY_EXISTS,
+                StatusCode: StatusCode.BAD_REQUEST,
+            }, res);
+            return;
+        }
+
+        // Create User    
+        const newUser = await UserService.create({
+            email: user.email as string,
+            display_name: user.display_name as string,
+            password: this.createPasswordHash(user.password as string),
+        });
+
+        res
+        .status(StatusCode.OK)
+        .json({
+            id: newUser.user_id,
+            email: newUser.email,
+            display_name: newUser.display_name,
+            created_on: newUser.created_on,
+        });
+    }
+
+    private static createPasswordHash(password: string): string {
+        const salt = bcrypt.genSaltSync();
+        return bcrypt.hashSync(password as string, salt);
+    }
+
+}
---
+++ Backend/src/core/service/user_service.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import Service from "./service";
+
+const client = new PrismaClient();
+
+interface UserCreateArgs {
+    email: string
+    display_name: string
+    password: string
+}
+
+export default class UserService extends Service {
+
+    public static getByEmail(email: string) {
+        return client.user.findUnique({
+            where: {
+                email
+            }
+        });
+    }
+
+    public static create(userArgs: UserCreateArgs) {
+        return client.user.create({
+            data: userArgs
+        });
+    }
+
+}
