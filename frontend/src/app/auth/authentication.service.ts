65368310524a5446893724b6ba8bd0505a8bf692
Paul Panosch <paulpanosch@gmx.at>
2023-05-01 23:59:09 +0200

    Created Login-Page. Added auth via Backend.

--- frontend/src/app/auth/authentication.service.ts
+++ frontend/src/app/auth/authentication.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { UserDTO } from './user-dto.model';
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Buffer } from 'buffer';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthenticationService {
 	constructor(private httpClient: HttpClient) {}
+
+	login(data: UserDTO): Observable<any> {
+		const credentials = `${data.email}:${data.password}`;
+
+		const headers = new HttpHeaders().set(
+			'Authorization',
+			`Basic ${Buffer.from(credentials).toString('base64')}`
+		);
+		return this.httpClient.post('/api/authorization/login', null, {
+			headers,
+		});
+	}
 
 	register(data: UserDTO): Observable<UserDTO> {
 		return this.httpClient.post('/api/authorization/register', data);
