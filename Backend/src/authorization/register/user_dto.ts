5def979062bdbffebd340b711987b3ccf8e307f2
Paul Panosch <paulpanosch@gmx.at>
2023-05-01 13:45:49 +0200

    /Regsiter now returns the UserDTO

--- Backend/src/authorization/register/user_dto.ts
+++ Backend/src/authorization/register/user_dto.ts
@@ -1,7 +1,8 @@
+import { User } from '@prisma/client';
 import DTO from '../../core/dto';
 
 export default class UserDTO extends DTO {
-    public readonly id?: number
+    public readonly id?: string
     public readonly email?: string
     public readonly display_name?: string
     public readonly password?: string
@@ -17,6 +18,22 @@ export default class UserDTO extends DTO {
         this.password = data.password;
         this.profile_picture = data.profile_picture;
         this.created_on = data.created_on;
+    }
+
+    /**
+     * Creates a UserDTO from the User object provided by prisma.
+     * 
+     * @param user User from prisma
+     * @returns UserDTO
+     */
+    public static createFromDB(user: User): UserDTO {
+        return new UserDTO({
+            id: user.user_id,
+            email: user.email,
+            display_name: user.display_name,
+            profile_picture: user.profile_picture ?? undefined,
+            created_on: user.profile_picture ?? undefined
+        });
     }
 
     /**
