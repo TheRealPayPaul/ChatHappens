c9a30be01f1c25502b1e99987749d9b5ad41b974
Paul Panosch <paulpanosch@gmx.at>
2023-05-03 18:00:27 +0200

    Refactored validator

--- Backend/src/core/dto/user/user_dto_validator.ts
+++ Backend/src/core/dto/user/user_dto_validator.ts
@@ -2,6 +2,12 @@ import { UserDTO } from './user_dto';
 import DTOValidator from '../dto_validator';
 
 export default class UserDTOValidator extends DTOValidator {
+
+    private static readonly MAX_EMAIL_LENGTH: number = 128;
+    private static readonly EMAIL_REGEX: RegExp = /^.+@.+$/;
+    private static readonly MAX_PASSWORD_LENGTH: number = 128;
+    private static readonly PASSWORD_REGEX: RegExp = /\s/g;
+
     /**
      * Checks if the data of the object is complete.
      * @required => email, display_name, password.
@@ -14,26 +20,26 @@ export default class UserDTOValidator extends DTOValidator {
         if (!user.email) {
             this.addError('"email" is missing!');
         } else {
-            if (!user.email.match(/^.+@.+$/)) {
+            if (!UserDTOValidator.EMAIL_REGEX.test(user.email)) {
                 this.addError('"email" is in the wrong format!');
             }
-            if (user.email.length > 128) {
-                this.addError('"email" exceeds the length limit of 128!');
+            if (user.email.length > UserDTOValidator.MAX_EMAIL_LENGTH) {
+                this.addError(`"email" exceeds the length limit of ${UserDTOValidator.MAX_EMAIL_LENGTH}!`);
             }
         }
 
         if (!user.display_name) {
             this.addError('"display_name" is missing!');
         } else {
-            if (user.display_name.length > 45) {
-                this.addError('"display_name" exceeds the length limit of 45!');
+            if (user.display_name.length > UserDTOValidator.MAX_PASSWORD_LENGTH) {
+                this.addError(`"display_name" exceeds the length limit of ${UserDTOValidator.MAX_PASSWORD_LENGTH}!`);
             }
         }
 
         if (!user.password) {
             this.addError('"password" is missing!');
         } else {
-            if (user.password.match(/\s/g)) {
+            if (UserDTOValidator.PASSWORD_REGEX.test(user.password)) {
                 this.addError('"password" has a white space!');
             }
         }
