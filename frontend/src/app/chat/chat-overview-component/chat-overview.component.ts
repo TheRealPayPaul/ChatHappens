18feeb2ec4455109226388a932a289f61cf4cc65
Paul Panosch <paulpanosch@gmx.at>
2023-05-04 15:07:33 +0200

    Added Chat page. Logo missing > waiting for logo bugfix. Chat page needs to be downscaled by 25%

---
+++ frontend/src/app/chat/chat-overview-component/chat-overview.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-chat-overview',
+	templateUrl: './chat-overview.component.html',
+	styleUrls: ['./chat-overview.component.scss'],
+})
+export class ChatOverviewComponent {}
---
+++ frontend/src/app/chat/chat-sidebar-component/chat-sidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-chat-sidebar',
+	templateUrl: './chat-sidebar.component.html',
+	styleUrls: ['./chat-sidebar.component.scss'],
+})
+export class ChatSidebarComponent {}
