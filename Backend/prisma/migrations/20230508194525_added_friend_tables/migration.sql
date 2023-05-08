47fdf93f3b1c24ac9584aa7df766cec52c842feb
Tobi <tstoderegger@gmail.com>
2023-05-08 21:47:12 +0200

    Created friend and friend request tables

---
+++ Backend/prisma/migrations/20230508194525_added_friend_tables/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE `FriendRequest` (
+    `friend_request_id` VARCHAR(191) NOT NULL,
+    `from_user_id` VARCHAR(191) NOT NULL,
+    `to_user_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`friend_request_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Friend` (
+    `friend_id` VARCHAR(191) NOT NULL,
+    `user_1_id` VARCHAR(191) NOT NULL,
+    `user_2_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`friend_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `FriendRequest` ADD CONSTRAINT `FriendRequest_from_user_id_fkey` FOREIGN KEY (`from_user_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `FriendRequest` ADD CONSTRAINT `FriendRequest_to_user_id_fkey` FOREIGN KEY (`to_user_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Friend` ADD CONSTRAINT `Friend_user_1_id_fkey` FOREIGN KEY (`user_1_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Friend` ADD CONSTRAINT `Friend_user_2_id_fkey` FOREIGN KEY (`user_2_id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
